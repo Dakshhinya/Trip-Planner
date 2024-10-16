3aa67d72055f87899f8acac5dda8b23dc8ca7ef5
Dakshhinya <dakku172004@gmail.com>
2024-10-17 01:03:39 +0530

    logo

--- ai-planner/src/App.jsx
+++ ai-planner/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
+import Hero from './components/ui/custom/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Button>hii</Button>
+    {/* Hero */}
+    <Hero/>
     </>
   )
 }
---
+++ ai-planner/src/components/ui/custom/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='pt-3 shadow-sm '>
+      <img src="/logo.svg" alt="logo" className='custom-logo' />
+    </div>
+  )
+}
+
+export default Header
